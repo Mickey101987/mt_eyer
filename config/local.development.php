93bd4d252c856924d65ac92f804b038bdfc6ff2f
Mickey101987 <75696755+Mickey101987@users.noreply.github.com>
2021-05-24 17:53:30 +0200

    Update local.development.php

--- config/local.development.php
+++ config/local.development.php
@@ -2,9 +2,13 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$dbopts = parse_url(getenv('postgres://sskoivfjangisv:314c34bf54ade499488035729c774893ab409a5dba5e6987e4c2a4922b5501af@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d9d638bvv247to'));
-
-// Database
-$settings['db']['database'] = ltrim($dbopts["path"], "/");
-$settings['pdo'] =  new \PDO('pgsql:host=$dbopts["host"];port=$dbopts["port"];dbname=ltrim($dbopts["path"],"/"), $dbopts["user"], $dbopts["pass"]);
-
+$db = parse_url(getenv("DATABASE_URL"));
+$settings['db']['database'] = '';
+$settings['pdo'] =  new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
