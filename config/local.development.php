4175d4b93219d89e0c281296c1b951efb409effa
Mickey101987 <75696755+Mickey101987@users.noreply.github.com>
2021-05-24 14:55:22 +0200

    Update local.development.php

--- config/local.development.php
+++ config/local.development.php
@@ -2,8 +2,9 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+$dbopts = parse_url(getenv('postgres://sskoivfjangisv:314c34bf54ade499488035729c774893ab409a5dba5e6987e4c2a4922b5501af@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d9d638bvv247to
 
 // Database
 $settings['db']['database'] = 'gmt_csp';
-$settings['pdo'] =  new \PDO('mysql:host=localhost;dbname=gmt_csp', 'root', '');
+$settings['pdo'] =  new \PDO('postgresql:host=$dbopts["host"];port=$dbopts["port"];dbname=ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
 
