da45d547e2dd5cbe618e8f5a0f019cf3f388fc39
Mickey101987 <75696755+Mickey101987@users.noreply.github.com>
2021-05-24 17:38:42 +0200

    Update local.development.php

--- config/local.development.php
+++ config/local.development.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$dbopts = parse_url(getenv('postgres://sskoivfjangisv:314c34bf54ade499488035729c774893ab409a5dba5e6987e4c2a4922b5501af@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d9d638bvv247to
+$dbopts = parse_url(getenv('postgres://sskoivfjangisv:314c34bf54ade499488035729c774893ab409a5dba5e6987e4c2a4922b5501af@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d9d638bvv247to'));
 
 // Database
 $settings['db']['database'] = ltrim($dbopts["path"], "/");
