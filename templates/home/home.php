36a73ce0a26ce76a866ac3d172b85ae37ac3f40f
Mickey101987 <75696755+Mickey101987@users.noreply.github.com>
2021-05-24 18:16:55 +0200

    Add files via upload

---
+++ templates/home/home.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.83.1">
+    <title>MT_Eyer - Home</title>
+    <!-- Bootstrap core CSS -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="/assets/css/cover.css" rel="stylesheet">
+</head>
+<body class="d-flex h-100 text-center text-white ">
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto pa">
+        <div>
+            <h3 class="float-md-start mb-0">MT_Eyer</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Contact</a>
+                <a class="nav-link" href="/admin/home">Admin</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="px-3">
+        <h1>MT_Eyer Administration</h1>
+        <h2 class="lead">Gestion centralis&eacute;e des transporteurs par motos-taxis.</h2>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary btn-connexion">Connexion Agent</a>
+        </p>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+        <p>powered by <a href="#" class="text-white">BakaTech</a> web from forest.</p>
+    </footer>
+</div>
+</body>
+</html>
